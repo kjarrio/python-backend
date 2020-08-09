f84b993fc73827f1da0866ddb5a872a1ce2db1ce
Kjartan Olason <olasonk@gmail.com>
2020-08-09 14:02:20 -0300

    Adicionando dados aleatórios para teste

--- escola/start.sh
+++ escola/start.sh
@@ -2,5 +2,6 @@
 python manage.py makemigrations
 python manage.py migrate
 python manage.py createsuperuser --username $DJANGO_SUPERUSER_USER --email $DJANGO_SUPERUSER_EMAIL --no-input
+python manage.py loaddata db.json
 python manage.py test
 python manage.py runserver 0.0.0.0:8000
