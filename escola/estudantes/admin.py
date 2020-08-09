618cd4ae2fc5b31a46ea0d250ca316469ab295ed
Kjartan Olason <olasonk@gmail.com>
2020-08-09 12:40:54 -0300

    Modelo básico para: Estudante

--- escola/estudantes/admin.py
+++ escola/estudantes/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from escola.estudantes.models import Estudante
 
-# Register your models here.
+admin.site.register(Estudante)
--- escola/estudantes/models.py
+++ escola/estudantes/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Estudante(models.Model):
+
+    id = models.AutoField(primary_key=True)
+    data_cadastro = models.DateField(auto_now_add=True)
+    nome = models.CharField(max_length=250, default='')
+    cpf = models.CharField(max_length=14, default='', unique=True)
+    rg = models.CharField(max_length=50, default='', unique=True)
+    email = models.CharField(max_length=250, default='')
+    celular = models.CharField(max_length=20, default='')
+    data_nascimento = models.DateField()
+
+    class Meta:
+        ordering = ['data_cadastro']
